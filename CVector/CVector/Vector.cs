dc6a2ab0fd93778b7718e1953ff05f8f0a1d9014
voroferrer <1dam.vferrer@institutoserpis.org>
2018-02-13 08:11:41 +0100

    Android Studio y CVector

--- CVector/CVector/Vector.cs
+++ CVector/CVector/Vector.cs
@@ -5,7 +5,26 @@ namespace CVector
 	public class Vector
 	{
 		public static void Sort(int[] v) {
-			Array.Sort<int> (v);
+			
+		}
+		/// <summary>
+		/// Obtiene el indice con el valor minimo
+		/// </summary>
+		/// <returns>The minimum.</returns>
+		/// <param name="v">V.</param>
+		/// <param name="initialIndex">Initial index.</param>
+		public static int IndexMin(int[] v, int initialIndex){
+
+		}
+		/// <summary>
+		/// Intercambia en el vector los elementos de index y otherIndex
+		/// </summary>
+		/// <param name="v">vector a modificar</param>
+		/// <param name="index">Indice del elemento</param>
+		/// <param name="otherIndex">Otro indice</param>
+		public static void Swap(int[] v, int index, int otherIndex){
+
+
 		}
 	}
 }
--- CVector/CVector/VectorTest.cs
+++ CVector/CVector/VectorTest.cs
@@ -13,6 +13,14 @@ namespace CVector
 			CollectionAssert.AreEqual (new int[]{ 7, 9, 12, 13, 15 }, v);
 
 		}
+		[Test]
+		public void IndexMin(){
+			int[] v = { 12, 9, 15, 7, 13 };
+			int indexMin = Vector.IndexMin (v, 4);
+			Assert.AreEqual (4, indexMin);
+		
+		
+		}
 	}
 }
 
