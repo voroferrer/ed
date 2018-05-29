6f7a79fb403db03668748aefbe38c815dfda2daa
voroferrer <1dam.vferrer@institutoserpis.org>
2018-05-29 18:07:28 +0200

    Añadido version sin modificar de CMinas

---
+++ CMinas/CMinas/Minas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CMinas
+{
+	public partial class Minas10 : Gtk.Window
+	{
+		public Minas10 () :
+			base (Gtk.WindowType.Toplevel)
+		{
+			this.Build ();
+		}
+	}
+}
+
---
+++ CMinas/CMinas/Widget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CMinas
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	public partial class Widget : Gtk.Bin
+	{
+		public Widget ()
+		{
+			this.Build ();
+		}
+	}
+}
+
